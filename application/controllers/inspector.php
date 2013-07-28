853ea82b93a5116237fdc9b31fee2bf5c85d0337
gary <gary_chen@channelwin.info>
2013-07-28 17:01:40 +0800

    v1.0.5.0(inspector name must more than five limit canceled)

--- application/controllers/inspector.php
+++ application/controllers/inspector.php
@@ -48,68 +48,52 @@ class Inspector extends CW_Controller
 	
 	public function add_username($str)
 	{
-		if(strlen($str) < 6)
+		if(preg_match("/^[a-zA-Z0-9]+$/", $str))
 		{
-			$this->form_validation->set_message('add_username', 'EmpNo should character or number,must more than six.');
-			return FALSE;
-		}
-		else
-		{
-			if(preg_match("/^[a-zA-Z0-9]+$/", $str))
+			$nameRecordObj = $this->db->query("SELECT a.username FROM inspector a WHERE a.username = '$str'");
+			if($nameRecordObj->num_rows() != 0)
 			{
-				$nameRecordObj = $this->db->query("SELECT a.username FROM inspector a WHERE a.username = '$str'");
-				if($nameRecordObj->num_rows() != 0)
-				{
-					$this->form_validation->set_message('add_username', 'This EmpNo already exists');
-					return FALSE;
-				}
-				else
-				{
-					return TRUE;
-				}
+				$this->form_validation->set_message('add_username', 'This EmpNo already exists');
+				return FALSE;
 			}
 			else
 			{
-				$this->form_validation->set_message('add_username', 'EmpNo should character or number must more than six.');
-				return FALSE;
+				return TRUE;
 			}
+		}
+		else
+		{
+			$this->form_validation->set_message('add_username', 'EmpNo is required and must character or number');
+			return FALSE;
 		}
 	}
 	//编辑时对用户名的验证
 	public function edit_username($str)
 	{
-		if(strlen($str) < 6)
+		if(preg_match("/^[a-zA-Z0-9]+$/", $str))
 		{
-			$this->form_validation->set_message('edit_username', 'EmpNo should character or number,must more than six.');
-			return FALSE;
-		}
-		else
-		{
-			if(preg_match("/^[a-zA-Z0-9]+$/", $str))
+			//取得当前id
+			$postUrl = $this->uri->uri_string();
+			$id = substr($postUrl, strripos($postUrl, "/")+1);
+			//查询id不等于当前，且测试站点名称等于当前输入的测试站点名称的记录数
+			$numObj = $this->db->query("SELECT COUNT(*) AS num FROM inspector ur WHERE ur.username = '$str' AND ur.id != '$id'");
+			$num = $numObj->first_row()->num;
+			//记录为空时允许修改，不为空时，不允许修改
+			if($num != 0)
 			{
-				//取得当前id
-				$postUrl = $this->uri->uri_string();
-				$id = substr($postUrl, strripos($postUrl, "/")+1);
-				//查询id不等于当前，且测试站点名称等于当前输入的测试站点名称的记录数
-				$numObj = $this->db->query("SELECT COUNT(*) AS num FROM inspector ur WHERE ur.username = '$str' AND ur.id != '$id'");
-				$num = $numObj->first_row()->num;
-				//记录为空时允许修改，不为空时，不允许修改
-				if($num != 0)
-				{
-					$this->form_validation->set_message('edit_username', 'This EmpNo already exists.');
-					return FALSE;
-				}
-				else
-				{
-					return TRUE;
-				}
+				$this->form_validation->set_message('edit_username', 'This EmpNo already exists.');
+				return FALSE;
 			}
 			else
 			{
-				$this->form_validation->set_message('edit_username', 'EmpNo should character or number must more than six.');
-				return FALSE;
+				return TRUE;
 			}
-		}	
+		}
+		else
+		{
+			$this->form_validation->set_message('edit_username', 'EmpNo is required and must character or number');
+			return FALSE;
+		}
 	}
 	
 	//对密码的格式校验
